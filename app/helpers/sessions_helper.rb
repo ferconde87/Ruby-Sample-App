79d03f99f7fc1e4c0535c4c4609c074f115fa307
fer <ferconde87@gmail.com>
2020-10-17 15:26:14 +0700

    Finish user edit, update and index

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,11 @@ module SessionsHelper
         end
     end
 
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user && user == current_user
+    end
+
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
@@ -35,7 +40,6 @@ module SessionsHelper
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
-    
 
     # Logs out the current user.
     def log_out
@@ -43,4 +47,16 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+    
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+    
 end
