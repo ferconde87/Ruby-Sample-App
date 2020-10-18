014c7148635646f2997e4957c152aa28e08f4c43
fer <ferconde87@gmail.com>
2020-10-18 21:23:07 +0700

    Finish user edit, update, index and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,21 +1,35 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  def setup
+    @user = users(:michael)
   end
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", contact_path, count: 1
+    assert_select "a[href=?]", users_path, count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get about_path
+    assert_select "title", full_title("About")
+    get help_path
+    assert_select "title", full_title("Help")
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    get root_path #I need to do a get to refresh the html, in other to see the links as a loggin user 
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", user_path(@user), count: 1
+    assert_select "a[href=?]", edit_user_path(@user), count: 1
+    assert_select "a[href=?]", logout_path, count: 1
+    #assert_select "a[href=?]", signup_path, count: 0 #this link should disappear after login
   end
 end
