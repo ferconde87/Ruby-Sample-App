ab52bc7df3479720dc98a3bcd3a31ebbc881924c
fer <ferconde87@gmail.com>
2020-10-24 10:30:11 +0700

    config storage using ENV variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on Amazon AWS.
+  # Store uploaded files on Google Cloud.
   config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain.
